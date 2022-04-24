f83d99601216f07713b19af1af24155ad583edcb
rubhern <rub3n.hernandez7@gmail.com>
2022-04-24 12:53:49 +0200

    updated web

--- client/src/App.js
+++ client/src/App.js
@@ -11,21 +11,15 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
+          <h1>FIB CALCULATOR KUBERNETES!</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </header>
+      
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
+        </header>
       </div>
     </Router>
   );
