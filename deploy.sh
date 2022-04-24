4ee605921a258d39095fa2c38d19ecd49c0c89dd
rubhern <rub3n.hernandez7@gmail.com>
2022-04-24 12:06:42 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t rubhern/multi-client:latest -t rubhern/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rubhern/multi-server:latest -t rubhern/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rubhern/multi-worker:latest -t rubhern/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push rubhern/multi-client:latest
+docker push rubhern/multi-worker:latest
+docker push rubhern/multi-server:latest
+
+docker push rubhern/multi-client:$SHA
+docker push rubhern/multi-worker:$SHA
+docker push rubhern/multi-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=rubhern/multi-server:$SHA
+kubectl set image deployments/client-deployment server=rubhern/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=rubhern/multi-worker:$SHA
